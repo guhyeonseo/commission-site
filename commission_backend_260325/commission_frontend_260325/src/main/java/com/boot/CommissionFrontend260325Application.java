9a54d6bf30a27fc9ceaf1be277e841c89e219fa0
guhyeonseo <guhamsi@naver.com>
2026-03-25 18:53:22 +0900

    백엔드 생성
    1

---
+++ commission_backend_260325/commission_frontend_260325/src/main/java/com/boot/CommissionFrontend260325Application.java
@@ -0,0 +1,13 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommissionFrontend260325Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommissionFrontend260325Application.class, args);
+	}
+
+}
